922a240f37e5c9e7dd26fc7479694e485d29965b
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-18 21:19:14 +0300

    Putting row data into the cells

--- myCheckList/CheckListViewController.swift
+++ myCheckList/CheckListViewController.swift
@@ -14,12 +14,12 @@ class CheckListViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
-    
+    //MARK: - Table View Data Sourse
     override func tableView(
       _ tableView: UITableView,
       numberOfRowsInSection section: Int
     ) -> Int {
-      return 1
+      return 5
     }
 
     override func tableView(
@@ -29,7 +29,24 @@ class CheckListViewController: UITableViewController {
       let cell = tableView.dequeueReusableCell(
         withIdentifier: "ChecklistItem",
         for: indexPath)
-      return cell
+        
+        // Add the following code
+          let label = cell.viewWithTag(1000) as! UILabel
+
+          if indexPath.row == 0 {
+            label.text = "Walk the dog"
+          } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+          } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+          } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+          } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+          }
+          // End of new code block
+
+          return cell
     }
 }
 
