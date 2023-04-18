4040966f41528692fa08f72c157cf7b34447da8d
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-18 20:57:17 +0300

    code for create one cell

--- myCheckList/CheckListViewController.swift
+++ myCheckList/CheckListViewController.swift
@@ -13,8 +13,24 @@ class CheckListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+      return 1
+    }
 
-
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: "ChecklistItem",
+        for: indexPath)
+      return cell
+    }
 }
 
 
