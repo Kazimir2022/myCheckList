69d0e9d95e1f917f378d1bd7adaadf2d7f596088
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-18 15:26:54 +0300

    Initial Commit

---
+++ myCheckList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myCheckList
+//
+//  Created by Kazimir on 18.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
