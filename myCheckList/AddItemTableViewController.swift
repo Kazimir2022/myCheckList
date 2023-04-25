f57d5196111cb701fa3b32442faa458f3e473449
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-25 16:54:55 +0300

    turn off large titles

--- myCheckList/AddItemTableViewController.swift
+++ myCheckList/AddItemTableViewController.swift
@@ -11,7 +11,7 @@ class AddItemTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.largeTitleDisplayMode = .never
         
     }
 
