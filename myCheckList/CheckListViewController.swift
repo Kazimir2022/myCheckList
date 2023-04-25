27f4c4cee2decfeb10cf6964063125673f85c0f0
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-25 12:38:58 +0300

    simplify the code - again

--- myCheckList/CheckListViewController.swift
+++ myCheckList/CheckListViewController.swift
@@ -9,12 +9,10 @@ import UIKit
 
 class CheckListViewController: UITableViewController {
     //MARK: - Initializer methods
-   var row0item = ChecklistItem()
-   var row1item = ChecklistItem()
-   var row2item = ChecklistItem()
-   var row3item = ChecklistItem()
-   var row4item = ChecklistItem()
   
+    var items = [ChecklistItem]()//«This is your new data model.
+    
+    
   //MARK:  type annotation
   // var row100:ChecklistItem
     
@@ -23,21 +21,28 @@ class CheckListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Add the following lines
-          row0item.text = "Walk the dog"
+        // Replace previous code with the following
+          let item1 = ChecklistItem()
+          item1.text = "Walk the dog"
+          items.append(item1)
 
-          row1item.text = "Brush my teeth"
-          row1item.checked = true
+          let item2 = ChecklistItem()
+          item2.text = "Brush my teeth"
+          item2.checked = true
+          items.append(item2)
 
-          row2item.text = "Learn iOS development"
-          row2item.checked = true
+          let item3 = ChecklistItem()
+          item3.text = "Learn iOS development"
+          item3.checked = true
+          items.append(item3)
 
-          row3item.text = "Soccer practice"
+          let item4 = ChecklistItem()
+          item4.text = "Soccer practice"
+          items.append(item4)
 
-          row4item.text = "Eat ice cream"
-          row4item.checked = true
-        
-        
+          let item5 = ChecklistItem()
+          item5.text = "Eat ice cream"
+          items.append(item5)
     }
     
     //MARK: - Table View Data Sourse
@@ -56,20 +61,14 @@ class CheckListViewController: UITableViewController {
         withIdentifier: "ChecklistItem",
         for: indexPath)
         
-        // Add the following code
-          let label = cell.viewWithTag(1000) as! UILabel//поиск конкретного вью с тегом и возвр ссылку на элемент
+        // получаем один элемент массива обектов(одну строку)
+        let item = items[indexPath.row]
         
-        if indexPath.row == 0 {
-            label.text = row0item.text
-          } else if indexPath.row == 1 {
-            label.text = row1item.text
-          } else if indexPath.row == 2 {
-            label.text = row2item.text
-          } else if indexPath.row == 3 {
-            label.text = row3item.text
-          } else if indexPath.row == 4 {
-            label.text = row4item.text
-          }
+        
+      
+          let label = cell.viewWithTag(1000) as! UILabel//поиск конкретного вью с тегом и возвр ссылку на элемент
+        label.text = item.text
+    
           // End of new code block
        configureCheckmark(for: cell, at: indexPath)
           return cell
@@ -82,17 +81,11 @@ class CheckListViewController: UITableViewController {
       didSelectRowAt indexPath: IndexPath
     ) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if indexPath.row == 0 {
-                row0item.checked.toggle()
-            } else if indexPath.row == 1 {
-                row1item.checked.toggle()
-            } else if indexPath.row == 2 {
-                row2item.checked.toggle()
-            } else if indexPath.row == 3 {
-                row3item.checked.toggle()
-            } else if indexPath.row == 4 {
-                row4item.checked.toggle()
-            }
+            // Replace everything inside this `if` condition
+            // with the following
+            let item = items[indexPath.row]
+            item.checked.toggle()
+
             configureCheckmark(for: cell, at: indexPath)
           }
           tableView.deselectRow(at: indexPath, animated: true)
@@ -103,32 +96,16 @@ class CheckListViewController: UITableViewController {
       for cell: UITableViewCell,
       at indexPath: IndexPath
     ) {
-      var isChecked = false
-
-      if indexPath.row == 0 {
-          isChecked = row0item.checked
-      } else if indexPath.row == 1 {
-        isChecked = row1item.checked
-      } else if indexPath.row == 2 {
-        isChecked = row2item.checked
-      } else if indexPath.row == 3 {
-        isChecked = row3item.checked
-      } else if indexPath.row == 4 {
-        isChecked = row4item.checked
+        // Replace full method implementation
+        let item = items[indexPath.row] // снова получаем один объект из массива объектов
+            // нет необходимости выводить др строки
+          if item.checked {
+            cell.accessoryType = .checkmark
+          } else {
+            cell.accessoryType = .none
+          }
       }
-
-      if isChecked {
-        cell.accessoryType = .checkmark
-      } else {
-        cell.accessoryType = .none
-      }
-    }
-    
-    
-    
-    
-    
-    
+            
 }
 
 
