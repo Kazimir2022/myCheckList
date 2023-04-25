88b9a603bb72762d5090541d51dac35b957bd569
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-25 16:49:27 +0300

    add new TableViewController

---
+++ myCheckList/AddItemTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddItemTableViewController.swift
+//  myCheckList
+//
+//  Created by Kazimir on 25.04.23.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
