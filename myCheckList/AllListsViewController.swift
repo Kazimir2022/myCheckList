3f3c7f8e5d6d156e638ff527ed29c29b74581ca2
Kazimir Makaranka <m2014ky@gmail.com>
2023-05-20 09:47:44 +0300

    Implement the tableView(_:cellForRowAt:)

--- myCheckList/AllListsViewController.swift
+++ myCheckList/AllListsViewController.swift
@@ -8,23 +8,27 @@
 import UIKit
 
 class AllListsViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-   
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-        return 0
-    }
-
-
+  let cellIdentifier = "ChecklistCell"
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  // MARK: - Table view data source
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 3
+  }
+  
+  override func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: cellIdentifier, for: indexPath)
+    cell.textLabel!.text = "List \(indexPath.row)"
+    return cell
+  }
+  
 }
