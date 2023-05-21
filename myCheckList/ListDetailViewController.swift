9ce919792cac3adec7f56e43d741c95e86b45d0b
Kazimir Makaranka <m2014ky@gmail.com>
2023-05-21 17:21:41 +0300

    add the other functionality

--- myCheckList/ListDetailViewController.swift
+++ myCheckList/ListDetailViewController.swift
@@ -43,4 +43,49 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
     super.viewWillAppear(animated)
     textField.becomeFirstResponder()
   }
+  // MARK: - Actions
+  @IBAction func cancel() {
+    delegate?.listDetailViewControllerDidCancel(self)
+  }
+  
+  @IBAction func done() {
+    if let checklist = checklistToEdit {
+      checklist.name = textField.text!
+      delegate?.listDetailViewController(
+        self,
+        didFinishEditing: checklist)
+    } else {
+      let checklist = Checklist(name: textField.text!)
+      delegate?.listDetailViewController(
+        self,
+        didFinishAdding: checklist)
+    }
+  }
+  // MARK: - Table View Delegates
+  override func tableView(
+    _ tableView: UITableView,
+    willSelectRowAt indexPath: IndexPath
+  ) -> IndexPath? {
+    return nil
+  }
+  
+  // MARK: - Text Field Delegates
+  func textField(
+    _ textField: UITextField,
+    shouldChangeCharactersIn range: NSRange,
+    replacementString string: String
+  ) -> Bool {
+    let oldText = textField.text!
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(
+      in: stringRange,
+      with: string)
+    doneBarButton.isEnabled = !newText.isEmpty
+    return true
+  }
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
 }
