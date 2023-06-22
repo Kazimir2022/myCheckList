0f036d9721aaa0d1343d4b5a382c47d0884bfe08
Kazimir Makaranka <m2014ky@gmail.com>
2023-06-22 13:10:46 +0300

    Sort the lists

--- myCheckList/Data Models/DataModel.swift
+++ myCheckList/Data Models/DataModel.swift
@@ -64,6 +64,7 @@ class DataModel {
         lists = try decoder.decode(
           [Checklist].self,
           from: data)
+        sortChecklists()
       } catch {
         print("Error decoding list array: \(error.localizedDescription)")
       }
@@ -89,4 +90,12 @@ class DataModel {
       userDefaults.set(false, forKey: "FirstTime")
     }
   }
+  
+  // MARK: - Sorting
+  func sortChecklists() {
+    lists.sort { list1, list2 in
+      return list1.name.localizedStandardCompare(list2.name) == .orderedAscending
+    }
+  }
 }
+
