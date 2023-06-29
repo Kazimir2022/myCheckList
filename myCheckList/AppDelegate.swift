91378f139b56801ae0466e416efe28ee500f19ac
Kazimir Makaranka <m2014ky@gmail.com>
2023-06-29 20:11:19 +0300

    Get permission to display local notifications

--- myCheckList/AppDelegate.swift
+++ myCheckList/AppDelegate.swift
@@ -13,7 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      let center = UNUserNotificationCenter.current()
+      center.requestAuthorization(options: [.alert, .sound]) {granted, error in
+        if granted {
+          print("We have permission")
+        } else {
+          print("Permission denied")
+        }
+      }
         return true
     }
 
